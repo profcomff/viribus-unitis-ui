d6cd338c25a1be10db7d2d14a91f761ee8729920
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2024-02-19 22:22:31 +0300

    Change config

--- app/src/config.ts
+++ app/src/config.ts
@@ -14,49 +14,16 @@ export default defineConfig({
             size: 250,
           },
           shape: 'square',
-          pagination: 'count',
           title: 'Viribus Unitis',
           description:
             '<b>Viribus Unitis</b> – Первое IT сообщество МГУ. Общаемся на айтишные темы и делаем полезные проекты вместе!<br />',
-          button: 'Дальше',
-        },
-
-        // "Полезные ссылки"
-        {
-          media: {
-            type: 'image',
-            src: import('./assets/img/vulogo.jpg'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Полезные ссылки',
-          description: '',
-          textAlign: 'center',
-          list: [
-            '<b><a href="https://to.profcomff.com/qmow1D">Приложение Твой ФФ!</a></b>',
-            '<a href="https://to.profcomff.com/profcomff-github">GitHub клуба</a>',
-          ],
-          button: 'Дальше',
-        },
-
-        // "Сообщество в других соцсетях"
-        {
-          media: {
-            type: 'image',
-            src: import('./assets/img/vulogo.jpg'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Сообщество в других соцсетях',
-          description: '',
-          textAlign: 'center',
-          list: [
-            '<a href="https://to.profcomff.com/viribus-vk">Мы в ВК</a>',
-            '<a href="https://to.profcomff.com/viribus-discord">Мы в Discord</a>',
-          ],
-          button: 'Дальше',
+            list: [
+              '<b><a href="https://to.profcomff.com/qmow1D">Приложение Твой ФФ!</a></b>',
+              '<a href="https://to.profcomff.com/profcomff-github">GitHub клуба</a>',
+              '<a href="https://to.profcomff.com/viribus-vk">Мы в ВК</a>',
+              '<a href="https://to.profcomff.com/viribus-discord">Мы в Discord</a>',
+            ],
+            button: 'Закрыть',
         },
       ],
     },
